7d12906125a4ef3b3161b18d2c1c3184e0c1c87c
Agutale-Arpit <arpitagutale947@gmail.com>
2025-02-11 17:11:45 +0530

    Building Stream UI

--- src/app/(root)/(home)/page.tsx
+++ src/app/(root)/(home)/page.tsx
@@ -1,9 +1,38 @@
 "use client";
 
+import ActionCard from "@/components/ActionCard";
+import { QUICK_ACTIONS } from "@/constants";
 import { useUserRole } from "@/hooks/useUserRole";
+import { useQuery } from "convex/react";
+import { api } from "../../../../convex/_generated/api";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import MeetingModal from "@/components/MeetingModal";
 
 export default function Home() {
-  const { isInterviewer, isCandidate } = useUserRole();
+  const router = useRouter();
+  const { isInterviewer, isCandidate, isLoading } = useUserRole();
+  const interviews = useQuery(api.interviews.getMyInterviews);
+
+  const [showModal, setShowModal] = useState(false);
+  const [modalType, setModalType] = useState<"start" | "join">();
+
+  const handleQuickAction = (title: string) => {
+    switch (title) {
+      case "New Call":
+        setModalType("start");
+        setShowModal(true);
+        break;
+      case "Join Interview":
+        setModalType("join");
+        setShowModal(true);
+        break;
+      default:
+        router.push(`/${title.toLowerCase()}`);
+    }
+  }
+
+  if (isLoading) return <p>loading...</p>
 
   return (
     <div className="container max-w-7xl mx-auto p-6">
@@ -21,7 +50,21 @@ export default function Home() {
 
       {isInterviewer ? (
         <>
-          <div className="grid sm:grid-cols-2 lg:grid-cols-4 gap-6">show sth here</div>
+          <div className="grid sm:grid-cols-2 lg:grid-cols-4 gap-6">
+            {QUICK_ACTIONS.map((action) => (
+              <ActionCard
+                key={action.title}
+                action={action}
+                onClick={() => handleQuickAction(action.title)}
+              />
+            ))}
+          </div>
+          <MeetingModal
+            isOpen={showModal}
+            onClose={() => setShowModal(false)}
+            title={modalType === "join" ? "Join Meeting" : "Start Meeting"}
+            isJoinMeeting={modalType === "join"}
+          />
         </>
       ) : (
         <>
