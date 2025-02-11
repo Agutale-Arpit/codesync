aea9ef6c91ddbcceb3fcd90abc27ea37d5f9741c
Agutale-Arpit <arpitagutale947@gmail.com>
2025-02-11 14:58:53 +0530

    Project Initialized

---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+        providers: [
+                {
+                        domain: "https://welcomed-amoeba-2.clerk.accounts.dev/",
+                        applicationID: "convex",
+                },
+        ]
+};
---
+++ src/app/constants/index.ts
@@ -0,0 +1,38 @@
+import { Code2 } from "lucide-react";
+
+export const INTERVIEW_CATEGORY = [
+  { id: "upcoming", title: "Upcoming Interviews", varient: "outline" },
+  { id: "completed", title: "Completed", variant: "secondary" },
+  { id: "succeeded", title: "Succeeded", variant: "default" },
+  { id: "failed", title: "Failed", variant: "destructive" },
+] as const;
+
+export const TIME_SLOTS = [
+  "09:00",
+  "09:30",
+  "10:00",
+  "10:30",
+  "11:00",
+  "11:30",
+  "12:00",
+  "12:30",
+  "13:00",
+  "13:30",
+  "14:00",
+  "14:30",
+  "15:00",
+  "15:30",
+  "16:00",
+  "16:30",
+  "17:00",
+];
+
+export const QUICK_ACTIONS = [
+  {
+    icon: Code2,
+    title: "New Call",
+    description: "Start an instant call",
+    color: "primary",
+    gradient: "from-primary/10 via-primary/5 to-transparent",
+  },
+]
