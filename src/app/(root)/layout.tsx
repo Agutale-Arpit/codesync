5024e118807a9d86797e0f4b3ece3ed649d9c2e1
Agutale-Arpit <arpitagutale947@gmail.com>
2025-02-12 17:16:12 +0530

    Meeting Setup UI Done

---
+++ src/app/(root)/layout.tsx
@@ -0,0 +1,7 @@
+import StreamClientProvider from "@/components/providers/StreamClientProvider";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <StreamClientProvider>{children}</StreamClientProvider>
+  )
+}
--- src/components/providers/StreamClientProvider.tsx
+++ src/components/providers/StreamClientProvider.tsx
@@ -20,7 +20,7 @@ const StreamVideoProvider = ({ children }: { children: ReactNode }) => {
         name: user?.firstName || "" + " " + user?.lastName || "" || user?.id,
         image: user?.imageUrl,
       },
-      tokenProvider: StreamVideoProvider,
+      tokenProvider: streamTokenProvider,
     });
 
     setStreamVideoClient(client);
